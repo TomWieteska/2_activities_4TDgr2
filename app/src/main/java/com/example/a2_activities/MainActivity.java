dea5e0f9b4d3d5cbd716f8438e19cf9cdc699a62
Tomasz Wieteska <tomasz.wieteska@elektrykplock.edu.pl>
2024-11-13 12:19:46 +0100

    Stan: Lifecycle of all two activities

--- app/src/main/java/com/example/a2_activities/MainActivity.java
+++ app/src/main/java/com/example/a2_activities/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a2_activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnRun;
+    String tag = "Zdarzenie: ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(tag,"onCreate(main)");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            btnRun = findViewById(R.id.btnRun);
+            btnRun.setOnClickListener(this);
+
             return insets;
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i = new Intent(this, MyNewActivity.class);
+        startActivity(i);
+
+    }
+    @Override
+    public void onStart(){
+
+        super.onStart();
+        Log.d(tag,"onStart(main)");
+    }
+    @Override
+    public void onRestart(){
+
+        super.onRestart();
+        Log.d(tag,"onRestart(main)");
+    }
+    @Override
+    public  void onResume(){
+
+        super.onResume();
+        Log.d(tag,"onResume(main)");
+
+    }
+    @Override
+    public  void onPause(){
+
+        super.onPause();
+        Log.d(tag,"onPause(main)");
+    }
+    @Override
+    public void onStop(){
+
+        super.onStop();
+        Log.d(tag,"onStop(main)");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag,"onDestroy(main)");
     }
 }
--- app/src/main/java/com/example/a2_activities/MyNewActivity.java
+++ app/src/main/java/com/example/a2_activities/MyNewActivity.java
@@ -1,6 +1,7 @@
 package com.example.a2_activities;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MyNewActivity extends AppCompatActivity {
 
+    private String tag = "Zdarzenie: ";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(tag,"onCreate(new)");
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_my_new);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +24,43 @@ public class MyNewActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onStart(){
+
+        super.onStart();
+        Log.d(tag,"onStart(new)");
+    }
+    @Override
+    public void onRestart(){
+
+        super.onRestart();
+        Log.d(tag,"onRestart(new)");
+    }
+    @Override
+    public  void onResume(){
+
+        super.onResume();
+        Log.d(tag,"onResume(new)");
+
+    }
+    @Override
+    public  void onPause(){
+
+        super.onPause();
+
+        Log.d(tag,"onPause(new)");
+    }
+    @Override
+    public void onStop(){
+
+        super.onStop();
+        Log.d(tag,"onStop(new)");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag,"onDestroy(new)");
     }
 }
